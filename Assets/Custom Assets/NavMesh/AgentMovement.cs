f5e8bee7597c2860a0723e24ca87284a572993c7
Matthias Kretschmayer <if20b196@technikum-wien.at>
2022-01-05 12:29:28 +0100

    Add navemesh agent movement

---
+++ Assets/Custom Assets/NavMesh/AgentMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Unity.LEGO.Minifig;
+using System.Linq;
+
+public class AgentMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    GameObject[] players;
+    // Start is called before the first frame update
+
+    public Transform goal;
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.updatePosition = true;
+        agent.SetDestination(goal.position);
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+    // Use Update method to set destination to player with highest pumpkin count
+    void Update() {
+        if(players.Length>0){
+            Debug.Log("WORKING");
+            GameObject maxPlayer = players.Aggregate((p1,p2) => p1.GetComponent<MinifigController>().pumpkinCount > p2.GetComponent<MinifigController>().pumpkinCount ? p1 : p2);
+            agent.SetDestination(maxPlayer.transform.position);
+        }else{
+            players = GameObject.FindGameObjectsWithTag("Player");
+            Debug.Log("SET  PLAYERS");
+        }
+    }
+    // Reset pumpkin count of player on collision
+    void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Player")){
+            var controller = other.GetComponent<MinifigController>();
+            controller.pumpkinCount = 0;
+        }
+    }
+}
